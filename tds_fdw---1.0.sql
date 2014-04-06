9ee1a699a06f84e72fe9f53cb7af45e1c07605f1
Geoff Montee <geoff.montee@gmail.com>
2014-04-06 09:21:49 -0400

    Added SQL file.

---
+++ tds_fdw---1.0.sql
@@ -0,0 +1,31 @@
+#*------------------------------------------------------------------
+#
+#				Foreign data wrapper for TDS (Sybase and Microsoft SQL Server)
+#
+# Author: Geoff Montee
+# Name: tds_fdw
+# File: tds_fdw/tds_fdw--1.0.sql
+#
+# Description:
+# This is a PostgreSQL foreign data wrapper for use to connect to databases that use TDS,
+# such as Sybase databases and Microsoft SQL server.
+#
+# This foreign data wrapper requires requires a library that uses the DB-Library interface,
+# such as FreeTDS (http://www.freetds.org/). This has been tested with FreeTDS, but not
+# the proprietary implementations of DB-Library.
+#----------------------------------------------------------------------------
+
+CREATE FUNCTION tds_fdw_handler()
+RETURNS fdw_handler
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION tds_fdw_validator(text[], oid)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FOREIGN DATA WRAPPER tds_fdw
+  HANDLER tds_fdw_handler
+  VALIDATOR tds_fdw_validator;
+  
