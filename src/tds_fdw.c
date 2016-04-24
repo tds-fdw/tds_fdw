091853f556699480b5e5e2352c1eac95bad2fe5c
Geoff Montee <geoff.montee@gmail.com>
2016-04-24 19:25:10 -0400

    Changed equality check (=) to less than check (<) in column number comparison.

--- src/tds_fdw.c
+++ src/tds_fdw.c
@@ -1382,7 +1382,7 @@ void tdsGetColumnMetadata(ForeignScanState *node, TdsFdwOptionSet *option_set)
 
 	festate->attinmeta = TupleDescGetAttInMetadata(node->ss.ss_currentRelation->rd_att);
 
-	if (option_set->match_column_names && festate->ncols != num_retrieved_attrs)
+	if (option_set->match_column_names && festate->ncols < num_retrieved_attrs)
 	{
 		ereport(ERROR,
 			(errcode(ERRCODE_FDW_INCONSISTENT_DESCRIPTOR_INFORMATION),
